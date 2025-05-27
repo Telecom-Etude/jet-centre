9785d68c0db555b3ab8035f7254b24725065ef4e
Allan Villars <allan.villars@telecom-paris.fr>
2025-05-27 11:33:00 +0200

    (vpo) add: cell used in the datatable

---
+++ src/app/(user)/(suivi)/manage/input-cell.tsx
@@ -0,0 +1,161 @@
+import { Textarea } from '@/components/ui/textarea';
+import { CellManyComboBox, CellSingleComboBox } from './cell-combobox';
+import { flexRender } from '@tanstack/react-table';
+import { Domain, StudyProgressStep } from '@prisma/client';
+
+interface InputCellProps {
+    cell_id: string;
+    data: any;
+    comp: any;
+    context: object;
+    admins: string[];
+    clientNames: string[];
+    clientEmails: string[];
+    codeToID: { [key: string]: string };
+}
+
+enum isSingleCombobox {
+    oc,
+    cconf,
+    qs,
+}
+
+enum isManyCombobox {
+    step,
+    refs,
+    cdps,
+    type_study,
+    client_name,
+    client_email,
+}
+
+enum isInput {
+    next_deadline,
+    date_pre_study,
+    last_check,
+    next_check,
+    gap,
+    end_rm,
+    pvrf,
+    av_number,
+}
+
+enum isTextarea {
+    title,
+    info,
+}
+
+interface ListItem {
+    [key: string]: string[];
+}
+
+interface ListId {
+    [key: string]: string;
+}
+
+export function InputCell({
+    cell_id,
+    data,
+    comp,
+    context,
+    admins,
+    clientNames,
+    clientEmails,
+    codeToID,
+}: InputCellProps) {
+    /*const [admins, setAdmins] = useState<string[]>([]);
+    const [clientNames, setClientNames] = useState<string[]>([]);
+    const [clientEmails, setClientEmails] = useState<string[]>([]);
+
+    useEffect(() => {
+        const rawAdmins = prisma.admin.findMany({
+            select: {
+                user: { select: { person: { select: { firstName: true, lastName: true } } } },
+            },
+        });
+        rawAdmins.then((value) =>
+            setAdmins(
+                value.map((admin) => admin.user.person.firstName + ' ' + admin.user.person.lastName)
+            )
+        );
+
+        const rawClients = prisma.client.findMany({
+            select: { person: { select: { firstName: true, lastName: true, email: true } } },
+        });
+        rawClients.then((value) => {
+            setClientNames(
+                value.map((client) => client.person.firstName + ' ' + client.person.lastName)
+            );
+            setClientEmails(value.map((client) => client.person.email));
+        });
+    }, [admins, clientNames, clientEmails]);*/
+
+    const step = Object.values(StudyProgressStep);
+    const type_study = Object.values(Domain);
+
+    const listItemManyComboBox: ListItem = {
+        step: step,
+        refs: admins,
+        cdps: admins,
+        type_study: type_study,
+        client_name: clientNames,
+        client_email: clientEmails,
+    };
+
+    const listItemSingleComboBox: ListItem = {
+        oc: ["Pas d'OC", 'Non envoyée', 'Envoyée client', 'Acceptée client'],
+        cconf: ['Oui', 'Non'],
+        qs: ['Non envoyé', 'Non concerné', 'Envoyé', 'Sans réponse', 'Reçu'],
+    };
+
+    const listId: ListId = {
+        step: 'Chercher un status',
+        refs: 'Chercher un.e référent.e',
+        cdps: 'Chercher des CDPs',
+        type_study: 'Chercher un type',
+        client_name: 'Chercher un client',
+        client_email: 'Chercher un mail client',
+        oc: 'Status OC',
+        cconf: 'Oui/Non',
+        qs: 'Status QS',
+    };
+
+    const id = cell_id.substring(2);
+    if (Object.values(isManyCombobox).includes(id)) {
+        return (
+            <CellManyComboBox
+                data={data}
+                items={listItemManyComboBox[id]}
+                placeholder={listId[id]}
+                codeToID={codeToID}
+            />
+        );
+    } else if (Object.values(isSingleCombobox).includes(id)) {
+        return (
+            <CellSingleComboBox
+                data={data}
+                items={listItemSingleComboBox[id]}
+                placeholder={listId[id]}
+                codeToID={codeToID}
+            />
+        );
+    } else if (Object.values(isInput).includes(id)) {
+        return <></>;
+    } else if (Object.values(isTextarea).includes(id)) {
+        return (
+            // Il faut prendre en compte le codeToID ici
+
+            <Textarea
+                className="h-full -min-h-[80px] -border resize-none text-center leading-snug -rounded-md px-4"
+                defaultValue={data}
+                onInput={(e) => {
+                    const target = e.currentTarget;
+                    target.style.height = 'auto';
+                    target.style.height = target.scrollHeight + 'px';
+                }}
+            />
+        );
+    } else {
+        return <div className="px-6">{flexRender(comp, context)}</div>;
+    }
+}
