d15b5587e5b6ea45a9e739a04ff019c6d47ee1d4
Allan Villars <allan.villars@telecom-paris.fr>
2025-05-27 11:31:09 +0200

    (vpo) add: datatable used to display informations on TS cells

---
+++ src/app/(user)/(suivi)/manage/data-table.tsx
@@ -0,0 +1,162 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { InputCell } from './input-cell';
+
+import {
+    ColumnDef,
+    ColumnFiltersState,
+    SortingState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from '@tanstack/react-table';
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import React from 'react';
+
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+    admins: string[];
+    clientNames: string[];
+    clientEmails: string[];
+    codeToID: { [key: string]: string };
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data,
+    admins,
+    clientNames,
+    clientEmails,
+    codeToID,
+}: DataTableProps<TData, TValue>) {
+    const [sorting, setSorting] = React.useState<SortingState>([]);
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        getFilteredRowModel: getFilteredRowModel(),
+        state: {
+            sorting,
+            columnFilters,
+        },
+    });
+
+    return (
+        <div>
+            <div className="flex items-center py-4">
+                <Input
+                    placeholder="Filter code..."
+                    value={(table.getColumn('code')?.getFilterValue() as string) ?? ''}
+                    onChange={(event) =>
+                        table.getColumn('code')?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm"
+                />
+            </div>
+            <div className="rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead
+                                            key={header.id}
+                                            className={
+                                                header.id === 'actions'
+                                                    ? 'max-w-[40px]'
+                                                    : 'min-w-[200px]'
+                                            }
+                                        >
+                                            {header.isPlaceholder ? null : (
+                                                <div className="px-2">
+                                                    {flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )}
+                                                </div>
+                                            )}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && 'selected'}
+                                >
+                                    {row.getVisibleCells().map((cell) => {
+                                        console.log(typeof cell.column.columnDef.cell);
+                                        console.log(cell.getValue());
+                                        return (
+                                            <TableCell key={cell.id} className="p-1">
+                                                <InputCell
+                                                    cell_id={cell.id}
+                                                    data={cell.getValue()}
+                                                    comp={cell.column.columnDef.cell}
+                                                    context={cell.getContext()}
+                                                    admins={admins}
+                                                    clientNames={clientNames}
+                                                    clientEmails={clientEmails}
+                                                    codeToID={codeToID}
+                                                />
+                                            </TableCell>
+                                        );
+                                    })}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    Previous
+                </Button>
+                <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    Next
+                </Button>
+            </div>
+        </div>
+    );
+}
