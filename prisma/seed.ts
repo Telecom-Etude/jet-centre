a1b55b0c30492ae7571d6de81b9f0779d2575334
Tom Webber <t.c.w.webber@gmail.com>
2025-08-01 19:29:25 +0200

    fix(prisma): better error handling in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const db = new PrismaClient();
 
-async function seed_dev() {
-    const email = process.env.ADMIN_EMAIL || 'example@telecom-etude.fr';
-    const position = process.env.ADMIN_POSITION || 'president';
+async function seedDev() {
+    const email = process.env.ADMIN_EMAIL;
+    if (!email) return console.error('ADMIN_EMAIL not provided. Skipping seed.');
 
-    await prisma.admin.create({
+    const position = process.env.ADMIN_POSITION ?? 'Info';
+
+    await db.admin.create({
         data: {
             position,
             user: {
@@ -24,19 +26,19 @@ async function seed_dev() {
     });
 }
 
-async function seed_prod() {
+async function seedProd() {
     const email = 'admin@telecom-etude.fr';
 
-    await prisma.admin.create({
+    await db.admin.create({
         data: {
-            position: 'respo-info',
+            position: 'Info',
             user: {
                 create: {
                     person: {
                         create: {
                             email,
-                            firstName: '',
-                            lastName: '',
+                            firstName: 'admin',
+                            lastName: 'admin',
                         },
                     },
                 },
@@ -46,17 +48,18 @@ async function seed_prod() {
 }
 
 async function main() {
-    if (process.env.NODE_ENV === 'production') return await seed_prod();
+    if (process.env.ENV === 'prod') return await seedProd();
+    if (process.env.ENV === 'dev') return await seedDev();
 
-    await seed_dev();
+    throw new Error(`Invalid ENV var in .env: expected 'prod' or 'dev', found ${process.env.ENV}`);
 }
 
 main()
     .then(async () => {
-        await prisma.$disconnect();
+        await db.$disconnect();
     })
     .catch(async (e) => {
         console.error(`#####\n${e}\n#####`);
-        await prisma.$disconnect();
+        await db.$disconnect();
         process.exit(1);
     });
